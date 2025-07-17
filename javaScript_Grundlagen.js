2f9e708999fa42d0de50751612be9b1b4bee6039
yi-joseph <yimiaoheidelberg@gmail.com>
2025-07-17 14:53:14 +0200

    Create javaScript_Grundlagen.js

---
+++ javaScript_Grundlagen.js
@@ -0,0 +1,12 @@
+// Hier werden die Grundlagen für JavaScript dargestellt. 
+// Variable deklarieren und initialisieren
+let name = "Yi";
+let alter = 33; 
+
+// Eine Funktion definieren
+function name() {
+  let name = "Yi",
+  return name; 
+}
+
+name();
